701f90da534ef39a8941559220c4a414568c139f
Hassen-GHAZEL <hassen0805@gmail.com>
2025-12-21 22:41:29 +0100

    Mise en place des signaux

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FooterComponent } from './components/static/footer/footer';
-import { LoaderComponent } from './components/tools/loader/loader';
-import { PopupComponent } from './components/tools/popup/popup';
+import { LoaderComponent } from './components/layout/loader/loader';
+import { PopupComponent } from './components/layout/popup/popup';
 import { HeaderComponent } from './components/static/header/header';
 
 @Component({
